fd3a9041a8ce6fb62c8d29431e01fbdae4d39d99
Marcus O'Shea <marcusoshea100@gmail.com>
2020-03-20 21:51:06 -0400

    Working local development environment
    Connects to postgres, able to update database with migrations file

---
+++ Helpers/DataContext.cs
@@ -0,0 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using WebApi.Entities;
+
+namespace WebApi.Helpers
+{
+    public class DataContext : DbContext
+    {
+        protected readonly IConfiguration Configuration;
+
+        public DataContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server database
+            options.UseNpgsql(Configuration.GetConnectionString("DbContext"));
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# HoldMyBeer
+.Net Core API for PokeBeer app.
+Setup Postgres docker instance for local development, will need postgres available for production version as well.
