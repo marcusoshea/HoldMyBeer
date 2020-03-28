a1e92d8b611c817a8850e8da34174e4cb0551785
Marcus O'Shea <marcusoshea100@gmail.com>
2020-03-27 22:31:43 -0400

    Corrected swagger api name

--- Startup.cs
+++ Startup.cs
@@ -104,7 +104,7 @@ namespace WebApi
             app.UseSwagger();
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "HoldMyBeer V1");
             });
 
             app.UseEndpoints(endpoints => endpoints.MapControllers());
